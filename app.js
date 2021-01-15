862ba75ea9d9c1f583baea94870720a316cf6659
FesoQue <adefesoq@gmail.com>
2021-01-15 03:34:31 +0100

    An ongoing landing page project

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav-item')
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
+
